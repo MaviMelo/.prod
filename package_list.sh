7db089545f31cf78a6ef2b576b58f97b1468dbde
Mavi <>
2025-11-21 12:50:36 -0300

    Lista de pacotes instalados manualmente no Linux.

---
+++ package_list.sh
@@ -0,0 +1,143 @@
+
+user@host:~$ pipx list
+venvs are in /home/work/.local/share/pipx/venvs
+apps are exposed on your $PATH at /home/work/.local/bin
+manual pages are exposed at /home/work/.local/share/man
+   package open-interpreter 0.4.3, installed using Python 3.12.3
+    - i
+    - interpreter
+    - interpreter-classic
+    - wtf
+   package pyzo 4.20.0, installed using Python 3.12.3
+    - pyzo
+   package sqlfluff 3.4.1, installed using Python 3.12.3
+    - sqlfluff
+   package vim-vint 0.3.21, installed using Python 3.12.3
+    - vint
+
+
+
+user@host:~$ sudo apt-mark showmanual
+bsdutils
+build-essential
+chromium-browser
+cmake
+composer
+curl
+dash
+diffutils
+efibootmgr
+exfat-fuse
+exfatprogs
+findutils
+flatpak
+fonts-firacode
+fonts-powerline
+g++
+gcc
+git
+gnome-tweaks
+gnome-user-docs-pt
+google-chrome-stable
+gparted
+grep
+grub-efi-amd64
+grub-efi-amd64-signed
+gzip
+hostname
+hping3
+hyphen-pt-br
+hyphen-pt-pt
+ibus-table-cangjie-big
+ibus-table-cangjie3
+ibus-table-cangjie5
+init
+language-pack-gnome-pt
+language-pack-gnome-pt-base
+language-pack-pt
+language-pack-pt-base
+libchewing3
+libchewing3-data
+libdouble-conversion3
+libgl1-mesa-glx
+liblua5.3-dev
+libm17n-0
+libmarisa0
+libopencc-data
+libopencc1.1
+libotf1
+libpinyin-data
+libpinyin15
+libqt5multimedia5
+libqt5sql5-sqlite
+libqt5sql5t64
+libqt5svg5
+libqt5webkit5
+libqt5xml5t64
+libreadline-dev
+libreoffice-help-common
+libreoffice-help-pt
+libreoffice-help-pt-br
+libreoffice-l10n-pt
+libreoffice-l10n-pt-br
+linux-generic-hwe-24.04
+linux-headers-6.11.0-29-generic
+login
+lua5.3
+m17n-db
+make
+mysql-server-8.0
+mythes-pt-br
+mythes-pt-pt
+ncurses-base
+ncurses-bin
+neofetch
+neovim
+net-tools
+nmap
+nodejs
+npm
+ntfs-3g
+obfs4proxy
+openjdk-21-jdk
+opera-stable
+packettracer
+pandoc
+pavucontrol
+php-cli
+php-mysql
+php-xml
+pipx
+python3-netifaces
+python3-passlib
+python3-pip
+python3-pydrive2
+radeontop
+shellcheck
+shim-signed
+sox
+texlive-fonts-recommended
+texlive-latex-extra
+texlive-luatex
+thunderbird-locale-pt
+thunderbird-locale-pt-br
+thunderbird-locale-pt-pt
+torbrowser-launcher
+tree
+ubuntu-desktop
+ubuntu-desktop-minimal
+ubuntu-minimal
+ubuntu-restricted-addons
+ubuntu-standard
+ubuntu-wallpapers
+unzip
+virtualbox
+virtualbox-dkms
+virtualbox-ext-pack
+virtualbox-qt
+wget
+whois
+xclip
+
+user@host:~$
+
